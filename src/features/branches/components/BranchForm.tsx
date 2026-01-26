83d29814d01b51e51e43511f1e339816c97962a3
Mozaffar <mozaffarmohammad@moi.sy>
2026-01-26 11:56:12 +0300

    main-c-2

--- src/features/branches/components/BranchForm.tsx
+++ src/features/branches/components/BranchForm.tsx
@@ -5,7 +5,7 @@ interface BranchFormProps {
   onSubmit: (values: BranchFormValues) => void;
   branch: BranchFormValues | undefined;
 }
-/// DEV
+/// MAIN_C
 export const BranchForm: React.FC<BranchFormProps> = ({ onSubmit, branch }) => {
   const [formData, setFormData] = useState<BranchFormValues>(
     branch || {

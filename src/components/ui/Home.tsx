28c867f2179efc08075d6d3570620885b20e9238
Mozaffar <mozaffarmohammad@moi.sy>
2026-01-26 11:11:27 +0300

    two-changes

--- src/components/ui/Home.tsx
+++ src/components/ui/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
       {/* <div className="text-center">
         <h1 className="text-4xl font-bold text-gray-900 mb-4 text-primary-500">مرحبا بك في موقعنا!</h1>
         <SignaturePad />
-      </div>
+      </div>  
       <Button label="Click me" variant={"primary"} size={"Small"} />  */}
       <Example />
     </div>
